d2ead956803186a2c97bfe99eacec07111114449
BakerAO <dpshunter@gmail.com>
2018-01-24 14:58:40 -0600

    npm installed axios, redux-promise, jquery@3, react-router-dom@4
    Signed-off-by: BakerAO <dpshunter@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
+import promise from 'redux-promise';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
-// 
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={PostsIndex} />
+                <Route path="/posts/:id" component={PostsShow} />
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('.container'));
