e66710e77f2ca48aa4f3cc1299741eed159c55e8
BakerAO <dpshunter@gmail.com>
2018-01-24 13:17:09 -0600

    Initial Commit
    Signed-off-by: BakerAO <dpshunter@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    disableHostCheck: true
+  }
+};
+
+// Edit node_modules/webpack-dev-server/bin/webpack-dev-server.js
+// to change host in new Server
