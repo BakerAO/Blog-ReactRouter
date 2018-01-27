9514ad4af75763cad44e31ca147c8ceab77cb980
BakerAO <dpshunter@gmail.com>
2018-01-27 10:34:45 -0600

    Added validation of form
    Signed-off-by: BakerAO <dpshunter@gmail.com>

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,20 +2,73 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field){
+        return (
+            <div className="form-group">
+            <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                {field.meta.error}
+            </div>
+        );
+    }
+
+    onSubmit(values){
+        // this === component 
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
             <div>
-                <form>
+                <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                     <Field
                         name="title"
-                        component={}
+                        label="Title"
+                        component={this.renderField}
                     />
+                    <Field
+                        name="categories"
+                        label="Categories"
+                        component={this.renderField}
+                    />
+                    <Field
+                        name="content"
+                        label="Post Content"
+                        component={this.renderField}
+                    />
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         );
     }
 }
 
+function validate(values){
+    const errors = {};
+
+    if (!values.title || values.title.length < 3){
+        errors.title = "Enter a title that is at least 3 characters";
+    }
+
+    if (!values.categories){
+        errors.categories = "Enter a category";
+    }
+
+    if (!values.content){
+        errors.content = "Enter some content";
+    }
+
+    // If erros is empty, the form is fine to submit
+    return errors;
+}
+
 export default reduxForm({
+    validate: validate,
     form: 'PostsNewForm'
 })(PostsNew);
